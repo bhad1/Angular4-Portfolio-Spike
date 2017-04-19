bdd0e3f3c3782c422955a2ae679eff097dadd0ec
bhadaway <bhadaway@bridge2solutions.com>
2017-04-19 17:04:42 -0400

    added color changer

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, EventEmitter, Output, OnChanges } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, OnChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ps-jumbotron',
@@ -9,6 +10,10 @@ export class JumbotronComponent implements OnInit {
   // first creation of output variable to determine which project to show
   @Output() showProjectNumber = new EventEmitter<number>();
 
+  //Primary color variable that will be passed to css to change entire project primary color
+  //Two-way data bound to the <input type=color> 
+  primaryColor= "#4CAF50";
+
   // this function is made so that (click) on each link can have a function to use to change the variable
   // and instead of storing it here just emit it out to the parent element
   // the parent element will catch it with (showProjectNumber)= "showProjectNumber($event)" as a parameter on the selector of this element
@@ -17,9 +22,15 @@ export class JumbotronComponent implements OnInit {
   }
 
   constructor() {
+
   }
 
   ngOnInit() {
+    console.log(this.primaryColor);
+  }
+
+  ngOnChanges() {
+    console.log(this.primaryColor);
   }
 
 }
